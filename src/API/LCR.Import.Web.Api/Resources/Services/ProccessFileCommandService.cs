2f03a2bf69f40e88b288c0c36cf89b849274182d
hypnotic <hypnotic-it@yandex.ru>
2019-11-03 13:57:38 +0300

    Add data validation

--- src/API/LCR.Import.Web.Api/Resources/Services/ProccessFileCommandService.cs
+++ src/API/LCR.Import.Web.Api/Resources/Services/ProccessFileCommandService.cs
@@ -1,7 +1,10 @@
 using ExcelDataReader;
+using LCR.Import.DataValidation;
 using LCR.TPM.Context;
+using Microsoft.Extensions.Logging;
 using System;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace LCR.Import.Web.Api.Resources
@@ -9,13 +12,16 @@ namespace LCR.Import.Web.Api.Resources
   public class ProccessFileCommandService : IProccessFileCommandService<ProccessFileCommand>
   {
     public ProccessFileCommandService(
-      TPMContext tpmCtx
+      TPMContext tpmCtx,
+      ILogger<ProccessFileCommandService> logger
       )
     {
       this.TPMContext = tpmCtx;
+      this.Logger = logger;
     }
 
     public TPMContext TPMContext { get; }
+    public ILogger<ProccessFileCommandService> Logger { get; }
 
     public async Task OnNextAsync(ProccessFileCommand command)
     {
@@ -32,6 +38,7 @@ namespace LCR.Import.Web.Api.Resources
           }
           do
           {
+            var validator = new ImportDataValidator();
             while (reader.Read())
             {
               var firstValue = reader.GetValue(0);
@@ -43,7 +50,21 @@ namespace LCR.Import.Web.Api.Resources
               var rawData = reader.ToImportRawDataModel();
               rawData.UploadHistoryId = command.ImportHistoryId;
 
-              this.TPMContext.ImportRawData.Add(rawData);
+              var result = validator.Validate(rawData);
+
+              if (!result.IsValid)
+              {
+                var errorCodes = result.Errors
+                  .Select(vf => Int32.Parse(vf.ErrorCode))
+                  .Aggregate((x, y) => x | y)
+                  ;
+
+                this.Logger.LogError("Data row {0} has validation errors. Codes {1}", rawData.DataRowId, errorCodes);
+              }
+              else
+              {
+                this.TPMContext.ImportRawData.Add(rawData);
+              }
             }
           } while (reader.NextResult());
         }
@@ -52,7 +73,7 @@ namespace LCR.Import.Web.Api.Resources
       var historyEntry = await this.TPMContext.ImportHistory.FindAsync(command.ImportHistoryId);
       historyEntry.Step = TPM.Model.ImportStep.Applied;
 
-      await this.TPMContext.SaveChangesAsync();
+      var res = await this.TPMContext.SaveChangesAsync();
     }
   }
 }
---
+++ src/BuildingBlocks/Validation/LCR.Import.DataValidation/ImportDataValidator.cs
@@ -0,0 +1,54 @@
+using FluentValidation;
+using FluentValidation.Validators;
+using LCR.TPM.Model;
+using System;
+
+namespace LCR.Import.DataValidation
+{
+  public class ImportDataValidator : AbstractValidator<ImportRawDataModel>
+  {
+    public ImportDataValidator()
+    {
+      RuleFor(c => c.DataRowId).Cascade(CascadeMode.StopOnFirstFailure)
+        .NotNull().WithErrorCode("2")
+        .Must(c => Int32.TryParse(c, out var res)).WithErrorCode("2");
+
+      RuleSet("Operator", () =>
+      {
+        Func<ImportRawDataModel, string, PropertyValidatorContext, bool> validationFunc = (model, val, ctx) =>
+        {
+          if (String.IsNullOrEmpty(model.SwitchOperatorName) && String.IsNullOrEmpty(model.PairedSwitchOperatorFullName))
+          {
+            return false;
+          }
+          return true;
+        };
+        RuleFor(c => c.SwitchOperatorName).Must(validationFunc).WithErrorCode("4");
+        RuleFor(c => c.PairedSwitchOperatorFullName).Must(validationFunc).WithErrorCode("4");
+      });
+
+      RuleFor(c => c.Direction).Cascade(CascadeMode.StopOnFirstFailure)
+        .NotNull().WithErrorCode("8")
+        .NotEmpty().WithErrorCode("8")
+        .Must(v => v.ToUpper() == "I" || v.ToUpper() == "O").WithErrorCode("8");
+
+      RuleSet("Dates", () =>
+      {
+        RuleFor(c => c.DateOpen).Cascade(CascadeMode.StopOnFirstFailure)
+          .NotNull().WithErrorCode("16")
+          .Must(val => DateTime.TryParse(val, out var dateOpen)).WithErrorCode("16");
+
+        RuleFor(c => c.DateClose)
+          .Must(val =>
+          {
+            if ((!String.IsNullOrEmpty(val) && !DateTime.TryParse(val, out var dateClose)))
+            {
+              return false;
+            }
+
+            return true;
+          }).WithErrorCode("32");
+      });
+    }
+  }
+}
